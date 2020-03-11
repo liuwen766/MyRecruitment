56606a136c5998968321a4fe0b212af82eb412d4
liuwen766 <780442619@qq.com>
2020-03-11 20:40:01 +0800

    海康威视 笔试

---
+++ src/com/liuwen/各公司笔试题/海康威视/Main1.java
@@ -0,0 +1,12 @@
+package com.liuwen.各公司笔试题.海康威视;
+
+/**
+ * @description: 手写一个单例模式
+ * @author: Liu Wen
+ * @create: 2020-03-11 20:05
+ **/
+public class Main1 {
+    /*
+    我写了一个静态内部类实现的额单例。
+     */
+}
