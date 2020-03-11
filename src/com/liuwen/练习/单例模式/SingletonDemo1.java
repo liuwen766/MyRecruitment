a498ced111cb41b976f6c586a81dcca935383a67
liuwen766 <780442619@qq.com>
2020-03-12 02:16:56 +0800

    单例模式

---
+++ src/com/liuwen/练习/单例模式/SingletonDemo1.java
@@ -0,0 +1,9 @@
+package com.liuwen.练习.单例模式;
+
+/**
+ * @description:     懒汉模式（线程安全）
+ * @author: Liu Wen
+ * @create: 2020-03-11 20:41
+ **/
+public class SingletonDemo1 {
+}
---
+++ src/com/liuwen/练习/单例模式/SingletonDemo3.java
@@ -0,0 +1,9 @@
+package com.liuwen.练习.单例模式;
+
+/**
+ * @description: Good good study,day day up!
+ * @author: Liu Wen
+ * @create: 2020-03-11 20:43
+ **/
+public class SingletonDemo3 {
+}
