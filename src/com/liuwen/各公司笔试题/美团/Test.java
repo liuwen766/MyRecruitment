e334f83bca4601372e624afc55c4428605655697
liuwen766 <780442619@qq.com>
2020-03-12 20:12:24 +0800

    美团的题也太难了吧

---
+++ src/com/liuwen/各公司笔试题/美团/Test.java
@@ -0,0 +1,12 @@
+package com.liuwen.各公司笔试题.美团;
+
+/**
+ * @description: Good good study,day day up!
+ * @author: Liu Wen
+ * @create: 2020-03-12 19:35
+ **/
+public class Test {
+    public static void main(String[] args){
+        System.out.println(3|2); //11|10
+    }
+}
