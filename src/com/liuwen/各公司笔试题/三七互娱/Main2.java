25467de9d16705883cf3d0ca1e3d1ba41a88a8ed
liuwen766 <780442619@qq.com>
2020-03-14 12:22:49 +0800

    三七互娱笔试

---
+++ src/com/liuwen/各公司笔试题/三七互娱/Main2.java
@@ -0,0 +1,21 @@
+package com.liuwen.各公司笔试题.三七互娱;
+
+/**
+ * @description:    不重复字符串的最长子串的长度，但必须含有字母a。没有返回0。
+ * @author: Liu Wen                 eg："bacdcdac"  返回 4   "bbbbb"  返回0
+ * @create: 2020-03-13 17:39
+ **/
+public class Main2 {
+    public static void main(String[] args){
+        String s = "bacdcdac";
+        int res = lengthOfLongestSubstring(s);
+        System.out.println(res);
+    }
+    /**
+     * @Description:
+     * @date 20.3.13 17:54
+     */
+    public static int lengthOfLongestSubstring(String s){
+        return 0;
+    }
+}
