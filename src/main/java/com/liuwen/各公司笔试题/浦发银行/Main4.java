f7e03645ab52d18326f16f7ef48d4b420389bd1a
liuwen766 <780442619@qq.com>
2020-03-31 19:55:43 +0800

    浦发第二次笔试

---
+++ src/main/java/com/liuwen/各公司笔试题/浦发银行/Main4.java
@@ -0,0 +1,14 @@
+package com.liuwen.各公司笔试题.浦发银行;
+
+/**
+ * @description:     浦发银行笔试第二批：没有算法题
+ * @author: Liu Wen
+ * @create: 2020-03-31 18:26
+ **/
+public class Main4 {
+    public static void main(String[] args){
+        /*
+        英语题30道、金融类题30道、中文阅读理解10道、图表题10道、看图题10道
+         */
+    }
+}
