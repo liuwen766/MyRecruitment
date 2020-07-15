ed71df783a7fbf94041cbd514969fb386468b05e
liuwen766 <780442619@qq.com>
2020-07-15 23:51:27 +0800

    Java 方法引用

---
+++ src/main/java/com/liuwen/Java8新特性/Character02MethodRef/Test01MethodRef.java
@@ -0,0 +1,30 @@
+package com.liuwen.Java8新特性.Character02MethodRef;
+
+/**
+ * ----------------------------------------------------------------------
+ * Copyright © 2014-2020 China Mobile (SuZhou) Software Technology Co.,Ltd.
+ * <p>
+ * The programs can not be copied and/or distributed without the express
+ * permission of China Mobile (SuZhou) Software Technology Co.,Ltd.
+ *
+ * @description:  方法引用
+ * @author: Create by Liu Wen at 2020-07-15 23:26
+ * ----------------------------------------------------------------------
+ **/
+
+public class Test01MethodRef {
+    /**
+     * @Description:  格式一：  对象::实例方法名
+     * @date 20.7.15 23:27
+     */
+
+    /**
+     * @Description:  格式二：  类::静态方法名
+     * @date 20.7.15 23:27
+     */
+
+    /**
+     * @Description:  格式三：  类::实例方法名
+     * @date 20.7.15 23:27
+     */
+}
