8142156bd82600b41fd78a62ab87e058ec99d925
liuwen766 <780442619@qq.com>
2020-07-16 14:33:56 +0800

    Java 8 新特性学习

---
+++ src/main/java/com/liuwen/Java8新特性/Character04Parallel/ForkJoinCalculate.java
@@ -0,0 +1,62 @@
+package com.liuwen.Java8新特性.Character04Parallel;
+
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * ----------------------------------------------------------------------
+ * Copyright © 2014-2020 China Mobile (SuZhou) Software Technology Co.,Ltd.
+ * <p>
+ * The programs can not be copied and/or distributed without the express
+ * permission of China Mobile (SuZhou) Software Technology Co.,Ltd.
+ *
+ * @description:     ForkJoin框架
+ * @author: Create by Liu Wen at 2020-07-16 11:19
+ * ----------------------------------------------------------------------
+ **/
+//继承RecursiveTask/RecursiveAction  使用fork/join框架
+public class ForkJoinCalculate extends RecursiveTask<Long>{
+
+    /**
+     * @Description:   版本信息号
+     */
+    private static final long serialVersionUID =121223232165456465L;
+
+    private long start;
+    private long end;
+
+    // 拆分门限值
+    private static final long threshold = 10000;
+
+    // 构造器
+    public ForkJoinCalculate(long start, long end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    /**
+     * @Description:  fork/join计算
+     * @date 20.7.16 13:55
+     */
+    @Override
+    public Long compute(){
+        long length = end - start;
+        long sum = 0L;
+        if(length<= threshold){
+            for (long i = start; i <= end; i++) {
+                sum += i;
+            }
+            return sum;
+        }else {
+            long middle = (end-start)/2+start;
+            ForkJoinCalculate left = new ForkJoinCalculate(start,middle);
+            left.fork();  //拆分子任务，同时压入线程队列
+
+            ForkJoinCalculate right = new ForkJoinCalculate(middle+1,end);
+            right.fork();
+            //获取子任务的计算结果
+            return left.join()+right.join();
+        }
+
+    }
+
+}
--- src/main/java/com/liuwen/Java8新特性/Character04Parallel/ForkJoinTest.java
+++ src/main/java/com/liuwen/Java8新特性/Character04Parallel/ForkJoinTest.java
@@ -1,5 +1,13 @@
 package com.liuwen.Java8新特性.Character04Parallel;
 
+import org.junit.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.stream.LongStream;
+
 /**
  * ----------------------------------------------------------------------
  * Copyright © 2014-2020 China Mobile (SuZhou) Software Technology Co.,Ltd.
@@ -13,4 +21,66 @@ package com.liuwen.Java8新特性.Character04Parallel;
  **/
 
 public class ForkJoinTest {
+
+    /**
+     * @Description:   使用ForkJoin框架
+     * @date 20.7.16 14:22
+     */
+    @Test
+    public void test01(){
+        //当前计时
+        Instant start = Instant.now();
+
+        //ForkJoin线程池用于执行fork/join线程子任务
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        //父类引用，实例化子类（多态？）
+        ForkJoinTask<Long> task = new ForkJoinCalculate(0,50000000000L);
+        //叫线程池来干活
+        Long sum = forkJoinPool.invoke(task);
+        System.out.println(sum);
+
+        //当前计时
+        Instant end = Instant.now();
+        //总耗时 毫秒
+        System.out.println("Forkjoin框架总耗时:"+Duration.between(start,end).toMillis()+"毫秒");
+
+    }
+
+    /**
+     * @Description:  使用传统方法
+     * @date 20.7.16 14:21
+     */
+    @Test
+    public void test02(){
+
+        Instant start = Instant.now();
+
+        long sum = 0;
+        for (long i = 0; i <= 50000000000L; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+
+        Instant end = Instant.now();
+        System.out.println("普通方法总耗时:"+Duration.between(start,end).toMillis()+"毫秒");
+    }
+
+    /**
+     * @Description:       Java8 新特性：并行流
+     * @date 20.7.16 14:28
+     */
+    @Test
+    public void test(){
+        Instant start = Instant.now();
+
+        long reduce = LongStream.rangeClosed(0, 50000000000L)
+                .parallel()
+                .reduce(0, Long::sum);
+        System.out.println(reduce);
+
+        Instant end = Instant.now();
+        System.out.println("Java并行流总耗时:"+Duration.between(start,end).toMillis()+"毫秒");
+    }
+
+
 }
