2011b3af9fb5cb5fbb1025b02a6e4012df789ae3
liuwen766 <780442619@qq.com>
2020-07-26 12:07:34 +0800

    SpringBoot整合Redis

---
+++ src/main/java/com/liuwen/redis/SpringBootRedisTest.java
@@ -0,0 +1,37 @@
+package com.liuwen.redis;
+
+import com.liuwen.redis.pojo.User;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * ----------------------------------------------------------------------
+ *
+ * @description: 所有的redis操作，其实对于java开发人员来说，十分的简单，更重要是要去理解redis的思想和每一种数
+ * 据结构的用处和作用场景！
+ * @author: Create by Liu Wen at  2020/7/26 11:51
+ * ----------------------------------------------------------------------
+ **/
+
+public class SpringBootRedisTest {
+
+  @Autowired
+  private RedisTemplate redisTemplate;
+
+  /**
+   * @Desc 对象需要序列化
+   * @param
+   * @return []
+   */
+  @Test
+  public void test(){
+    User user = new User("liuwen",26);
+    redisTemplate.opsForValue().set("user",user);
+    Object student1 = redisTemplate.opsForValue().get("user");
+    System.out.println(student1);
+  }
+
+
+
+}
---
+++ src/main/java/com/liuwen/redis/pojo/User.java
@@ -0,0 +1,26 @@
+package com.liuwen.redis.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+/**
+ * ----------------------------------------------------------------------
+ *
+ * @description: 对象需要序列化
+ * @author: Create by Liu Wen at  2020/7/26 11:49
+ * ----------------------------------------------------------------------
+ **/
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+
+  private String name;
+  private Integer age;
+}
