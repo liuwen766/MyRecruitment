9709258fcfd372a631a52388545e0465a8fc82e8
liuwen766 <780442619@qq.com>
2020-11-02 09:06:56 +0800

    2020.11.2

--- src/main/java/com/liuwen/练习/V202话单/TestDigOrder.java
+++ src/main/java/com/liuwen/练习/V202话单/TestDigOrder.java
@@ -1,6 +1,12 @@
 package com.liuwen.练习.V202话单;
 
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Stream;
 
 /**
  * ----------------------------------------------------------------------
@@ -17,9 +23,34 @@ package com.liuwen.练习.V202话单;
  * <p>
  * ----------------------------------------------------------------------
  **/
+@Slf4j
 public class TestDigOrder {
+
+    static AtomicLong atomicLong = new AtomicLong(195);
 
     public static void main(String[] args) {
 
+
+        System.out.println("---------------");
+        log.error("第{}次话单上传{}至FTP服务器失败", 1, "fileName");
+        System.out.println("---------------");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(getBossStreamNumber());
+        }
+
+
     }
+
+    public static String getBossStreamNumber() {
+        if (atomicLong.weakCompareAndSet(199,100)) {
+            atomicLong = new AtomicLong(100);
+        }
+        long ebossStreamNumber = atomicLong.getAndIncrement();
+        long result = ebossStreamNumber % 999L;
+        result = result < 0 ? result + 999L : result;
+        return String.format("%03d", result + 1);
+    }
+
+
 }
