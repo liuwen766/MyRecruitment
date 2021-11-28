cecc2011ef504731aa77792b1f1a159ff6cb4092
liuwen2 <liuwen2@cmss.chinamobile.com>
2021-11-28 10:51:48 +0800

    Java面试第一、二、三季，start。

---
+++ src/main/java/com/liuwen/面试第三季/LRULFU/LRUCacheDemo.java
@@ -0,0 +1,20 @@
+package com.liuwen.面试第三季.LRULFU;
+
+/**
+ * @desc: 手写LRU
+ * @data: 2021.11.28 10:29
+ */
+public class LRUCacheDemo {
+    //step1：构建一个Node节点作为数据载体
+
+    //step2：构建双向队列，作为Node载体
+
+    //step3：完成读→get()，写→put()
+
+
+    // test
+    public static void main(String[] args) {
+
+    }
+}
+
